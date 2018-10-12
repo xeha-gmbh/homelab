a75ff26b716f75c83edfc357117ed4d76b32a4a8
Weinan Qiu <davidiamyou@gmail.com>
2018-10-12 15:49:05 -0400

    create lab command

---
+++ lab.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/imulab/homelab/proxmox"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"os"
+)
+
+func NewLabCommand() *cobra.Command {
+	cmds := &cobra.Command{
+		Use:   "lab",
+		Short: "lab: easily configure the lab environment",
+	}
+
+	cmds.ResetFlags()
+	cmds.AddCommand(proxmox.NewProxmoxCommand())
+
+	return cmds
+}
+
+func Run() error {
+	// TODO pflag.CommandLine.SetNormalizeFunc()
+
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+
+	cmd := NewLabCommand()
+	return cmd.Execute()
+}
+
+func main()  {
+	if err := Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
